6ffca0384fca2461dc953d7f25d7b9bda4db5a7f
donpsabance <donabance@Dons-MacBook-Pro.local>
2019-01-30 06:43:48 -0800

    namespace

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -2,49 +2,44 @@
 #include <string>
 #include <vector>
 
-using namespace std;
-
-void game();
-bool check_game(vector<char> vec);
-bool valid_input(vector<char> vec, string str);
-void draw_board(vector<char> vec);
+void run_game();
+bool check_game(std::vector<char> vec);
+bool valid_input(std::vector<char> vec, std::string str);
+void draw_board(std::vector<char> vec);
 void clear_screen();
 
 int main(){
 
-	game();
+	run_game();
 	return 0;
 }
 
-void game(){
+void run_game(){
 
-	vector<char> table(9, '~');
-	string location = "0";
+	std::vector<char> table(9, '~');
+	std::string location = "0";
 	char player_turn = 'X'; //first player to start
 
   	clear_screen();
   	draw_board(table);
   	while(!check_game(table)){
-
-  		cout << "enter location for player (" << player_turn << "): ";
-  		cin >> location;
-
   		while(!valid_input(table, location)){
-  			cout << "invalid location, please enter again: (" << player_turn << "): ";
-  			cin >> location;
+  			std::cout << "enter location (1-9) for player (" << player_turn << "): ";
+  			std::cin >> location;
   		}
 
   		table[stoi(location) - 1] = player_turn;
   		clear_screen();
   		draw_board(table);
 
-  		if(check_game(table)){ cout << "player " << player_turn << " has won" << endl;}
+  		std::cout << "\nprevious move: " << location << "\n";
+  		if(check_game(table)){ std::cout << "player " << player_turn << " has won \n";}
   		(player_turn == 'X') ? player_turn = 'O' : player_turn = 'X';
 
   	}
 }
 
-bool check_game(vector<char> vec){
+bool check_game(std::vector<char> vec){
 
 	bool result = false;
 	bool draw = true;
@@ -58,52 +53,46 @@ bool check_game(vector<char> vec){
 	else if((vec[2] == vec[5] && vec[5] == vec[8]) && vec[2] != '~') {result = true;}
 	else if((vec[0] == vec[4] && vec[4] == vec[8]) && vec[0] != '~') {result = true;}//diagon
 	else if((vec[2] == vec[4] && vec[4] == vec[6]) && vec[2] != '~') {result = true;}
-
-	//check for draw
-	for(int i = 0; i < vec.size(); i++){
-		if(vec[i] == '~'){
-			draw = false;
-			break;
-		}
-
-		if(i == vec.size() - 1){
-			if(draw){
-				cout << "game ended in a draw \n";
-				exit(0);
+	else{
+		//check for draw
+		for(int i = 0; i < vec.size(); i++){
+			if(vec[i] == '~'){ 
+				draw = false; 
+				break; 
+			}
+			if(i == vec.size() - 1){
+				if(draw){ std::cout << "game ended in a draw \n"; exit(0); }
 			}
 		}
 	}
-
 	return result;
 }
 
-bool valid_input(vector<char> vec, string str){
+bool valid_input(std::vector<char> vec, std::string str){
 
 	bool result = false;
 	try{ 
-
 		int x = stoi(str);
 		if(x > 0 && x <= 9){  (vec[x - 1] == '~') ? result = true : result = false; }
 		return result;
-
 	}catch(...){ result = false;}
 	return result;
 }
 
-void draw_board(vector<char> vec){
+void draw_board(std::vector<char> vec){
 
-	cout << "       |       |		 " << endl; 
-	cout << "   " << vec[0] << "   |   " << vec[1] << "   |   " << vec[2] << "	 " << endl; 
-	cout << "       |       |		 " << endl; 
-	cout << " - - - + - - - + - - -	 " << endl; 
-	cout << "       |       |		 " << endl; 
-	cout << "   " << vec[3] << "   |   " << vec[4] << "   |   " << vec[5] << "	 " << endl; 
-	cout << "       |       |		 " << endl; 
-	cout << " - - - + - - - + - - -	 " << endl; 
-	cout << "       |       |		 " << endl; 
-	cout << "   " << vec[6] << "   |   " << vec[7] << "   |   " << vec[8] << "	 " << endl; 
-	cout << "       |       |		 " << endl; 
+	std::cout << "       |       |		 \n"; 
+	std::cout << "   " << vec[0] << "   |   " << vec[1] << "   |   " << vec[2] << "	 \n"; 
+	std::cout << "       |       |		 \n"; 
+	std::cout << " - - - + - - - + - - -	 \n"; 
+	std::cout << "       |       |		 \n"; 
+	std::cout << "   " << vec[3] << "   |   " << vec[4] << "   |   " << vec[5] << "	 \n"; 
+	std::cout << "       |       |		 \n"; 
+	std::cout << " - - - + - - - + - - -	 \n"; 
+	std::cout << "       |       |		 \n"; 
+	std::cout << "   " << vec[6] << "   |   " << vec[7] << "   |   " << vec[8] << "	 \n"; 
+	std::cout << "       |       |		 \n"; 
 
 }
 
-void clear_screen(){ cout << "\033[2J\033[1;1H"; }
+void clear_screen(){ std::cout << "\033[2J\033[1;1H"; }
